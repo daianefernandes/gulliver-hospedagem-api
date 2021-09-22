52c3ec02085c961e5f7d2997b3b78dbcd87ee189
daianefernandes <daianedsdaia@gmail.com>
2021-09-22 08:21:19 -0300

    ajustes no caminho dos diretorios

--- js/geolocation.js
+++ js/geolocation.js
@@ -5,7 +5,8 @@ const geolocationVerify = () => {
         const divCoordinates = document.getElementById("coordinates");
         const divMap = document.getElementById("map");
         const { latitude , longitude } = position.coords;
-        
+        console.log(divCoordinates);
+
         const map = new Image();
         map.src = `https://www.mapquestapi.com/staticmap/v5/map?key=AiGieBGXcABDbOM4btILi2sK7mmcxcTV&locations=${latitude},${longitude}&size=400,300`;
         divMap.appendChild(map);
@@ -13,7 +14,7 @@ const geolocationVerify = () => {
 
     const errorPosicion = (error) => {
         const divCoordinates = document.getElementById("coordinates");
-        console.log(error.code, error.message);
+        console.log(divCoordinates + error.code, error.message);
     };
 
     navigator.geolocation.getCurrentPosition( success , errorPosicion );
@@ -22,7 +23,7 @@ const geolocationVerify = () => {
 const verifyNavigator = () => {
     if(!navigator.geolocation){
         const divCoordinates = document.getElementById("coordinates");
-        console.log("localização não suportada pelo navegador")
+        console.log(divCoordinates + "localização não suportada pelo navegador")
         return;
     }
     geolocationVerify();
